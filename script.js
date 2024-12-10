0ac39f42dfbd53a01580128f2500f6a306da83d2
Bradley Laney <bradley.laney@gmail.com>
2024-12-10 14:35:01 -0500

    mobile fix

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 class HeaderBar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-        <header>
-        <div style="overflow:hidden">
-          <span style="float:left">
-            <a href="/">home</a>
-          </span>
-          <span style="float:right">
-            <a href="https://github.com/stoksc">github</a>
-            <a href="https://www.linkedin.com/in/bradleylaney/">linkedin</a>
-          </span>
-        </div>
+      <header>
+        <a href="/">home</a>
+        <span style="float:right;">
+          <a href="https://github.com/stoksc">github</a>
+          <a href="https://www.linkedin.com/in/bradleylaney/">linkedin</a>
+        </span>
       </header>
 `;
   }
